59926cb7af1a13efd876f21987679fd2a4d8609b
Justas Gražulevičius <91288874+jusgra@users.noreply.github.com>
2023-08-14 23:10:56 +0300

    added canva size selections, small drawing bug that occurs when you rapidly move and click your mouse on canva

--- drawColors.js
+++
@@ -1,1 +0,0 @@
-export default Cab = ["black", "white", "blue", "red", "orange", "yellow", "green", "brown", "chocolate", "pink", "lime"];
--- index.js
+++ index.js
@@ -1,53 +1,89 @@
-//import Cab from "/drawColors.js";
-
 const colors = ["black", "white", "blue", "red", "orange", "yellow", "green", "brown", "chocolate", "pink", "lime"];
-
-initColors();
-
-const drawGrid = (size) => {
-  const table = document.getElementById("draw-grid");
-  for (let i = 0; i < size; i++) {
-    const tableRow = table.appendChild(document.createElement("tr"));
-    for (let i = 0; i < size; i++) {
-      tableRow.appendChild(document.createElement("td"));
-    }
-  }
-};
+const canvaSizeSelections = [10, 15, 20, 25];
 
 let gridSize = 10;
 let mousePressed = false;
 let drawColor = "red";
 
+initPalette();
+initButtons();
 drawGrid(gridSize);
+addListeners();
 
-const cell = document.querySelectorAll("td");
+// document.querySelector("button").addEventListener("click", () => {
+//   eraseGrid();
+//   gridSize = 20;
 
-for (const n of cell) {
-  n.addEventListener("click", () => {
-    n.style.background = drawColor;
-    mousePressed = !mousePressed;
-  });
-  n.addEventListener("mouseover", () => {
-    if (mousePressed) n.style.background = drawColor;
-    console.log(mousePressed);
-  });
-  n.addEventListener("mousedown", () => {
-    mousePressed = !mousePressed;
-    console.log("mouseDown");
-  });
-  n.addEventListener("mouseup", () => {
-    mousePressed = !mousePressed;
-    console.log("mouseUp");
-  });
+//   drawGrid();
+
+//   addListeners();
+// });
+
+// document.querySelector(".canva-size").addEventListener("input", (e) => {
+//   //console.log(e.target.value);
+// });
+
+function addListeners() {
+  const cell = document.querySelectorAll("td");
+  for (const n of cell) {
+    n.addEventListener("click", () => {
+      console.log(mousePressed);
+      n.style.background = drawColor;
+      //mousePressed = !mousePressed;
+    });
+    n.addEventListener("mouseover", () => {
+      if (mousePressed) n.style.background = drawColor;
+      console.log(mousePressed);
+    });
+    n.addEventListener("mousedown", () => {
+      mousePressed = !mousePressed;
+    });
+    n.addEventListener("mouseup", () => {
+      mousePressed = !mousePressed;
+    });
+  }
 }
 
-function initColors() {
-  const colorPallet = document.getElementById("colors");
+function drawGrid() {
+  console.log(gridSize);
+  const table = document.getElementById("canva");
+  for (let i = 0; i < gridSize; i++) {
+    const tableRow = table.appendChild(document.createElement("tr"));
+    for (let i = 0; i < gridSize; i++) {
+      tableRow.appendChild(document.createElement("td"));
+    }
+  }
+}
+
+function eraseGrid() {
+  const canva = document.getElementById("canva");
+  for (let i = 0; i < gridSize; i++) {
+    canva.removeChild(canva.firstChild);
+  }
+}
+
+function initPalette() {
+  const colorPalette = document.getElementById("color-palette");
   for (const single of colors) {
-    const div = colorPallet.appendChild(document.createElement("div"));
+    const div = colorPalette.appendChild(document.createElement("div"));
     div.classList.add(single);
     document.querySelector("." + single).addEventListener("click", () => {
       drawColor = single;
+    });
+  }
+}
+
+function initButtons() {
+  const buttonsDiv = document.getElementById("buttons");
+  for (let i = 0; i < canvaSizeSelections.length; i++) {
+    const singleButton = buttonsDiv.appendChild(document.createElement("button"));
+
+    singleButton.textContent = canvaSizeSelections[i] + "x" + canvaSizeSelections[i];
+    singleButton.addEventListener("click", () => {
+      eraseGrid();
+      gridSize = canvaSizeSelections[i];
+      drawGrid();
+      addListeners();
     });
   }
 }
